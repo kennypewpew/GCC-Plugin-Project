6ca882f25c994bf70ce46246cb898ad27e9b818f
Kenny P <ken.peou@gmail.com>
2014-10-20 11:53:59 +0200

    A few extra things to test for

--- tests/pragma.c
+++ tests/pragma.c
@@ -2,7 +2,20 @@
 
 
 
-#pragma MIHPS vcheck test
+#pragma MIHPS vcheck (f1, f2, f2, f4)
+
+void f1() {
+  return;
+}
+void f2() {
+  return;
+}
+void f3() {
+  return;
+}
+void f4() {
+  return;
+}
 
 int main(int argc, char **argv) {
 
