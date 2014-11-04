698a9c3e522fd4313cbcbb1d2c7a03c6f776a38c
Kenny P <ken.peou@gmail.com>
2014-11-04 17:48:40 +0100

    Adds print statements to start of loops

--- src/vectorization-plugin.cpp
+++ src/vectorization-plugin.cpp
@@ -2,12 +2,18 @@
 
 #include <tree.h>
 #include <tree-pass.h>
+#include <basic-block.h>
+
+#include "includes.hpp"
+
 #include <context.h>
 #include <input.h>
 #include <c-family/c-pragma.h>     // c_register_pragma
 #include <function.h>              // cfun
 #include <diagnostic.h>            // error(...)
 #include <intl.h>                  // G_(...)
+
+//#include <builtins.h>
 
 #include <iostream>
 #include <string>
@@ -28,7 +34,7 @@ void check_instr_args_for_doubles() {
     }
     if ( flag == true ) {
       warning (OPT_Wpragmas,
-	       "Function \"%s\" declared to be checked multiple times",
+	       "[pragma] Function \"%s\" declared to be checked multiple times",
 	       instr_args[i]);
       arg_used[i] = true;
     }
@@ -48,11 +54,13 @@ bool function_to_check(const char *fn) {
 
 
 static void test_if_all_used(void *event_data, void *data) {
-  printf("Testing if all %d functions were checked\n", arg_used.length());
+  printf("[pragma] Testing if all %d functions were checked\n", 
+	 arg_used.length());
+  // Returns wrong file number as error
   for ( int i = 0 ; i < arg_used.length() ; ++i )
     if ( !arg_used[i] ) 
       warning (OPT_Wpragmas,
-	       "Function \"%s\" declared to analyze, but not found",
+	       "[pragma] Function \"%s\" declared to analyze, but not found",
 	       instr_args[i]);
 }
 /******* End analysis storage/functions ******/
@@ -83,7 +91,7 @@ static void vcheck_pragma_handler(cpp_reader *ARG_UNUSED(notUsed)) {
   else {
     while ( tmpType == CPP_NAME) {
       current_string = IDENTIFIER_POINTER (tmpTree);
-      printf("Function \"%s\" recognized as needing analysis\n",current_string);
+      printf("[pragma] Function \"%s\" recognized as needing analysis\n",current_string);
 
       instr_args.safe_push(current_string);
       arg_used.safe_push(false);
@@ -107,13 +115,56 @@ static void vcheck_pragma_handler(cpp_reader *ARG_UNUSED(notUsed)) {
 }
 
 static void register_vector_pragmas(void *even_data, void *data) {
-  printf("Registering vectorization pragmas\n");
+  printf("[pragma] Registering vectorization pragmas\n");
   c_register_pragma("MIHPS", "vcheck", vcheck_pragma_handler);
 }
 
 /******* End pragma handling ********/
 
 /****** Begin new pass *********************/
+
+void placeholder(basic_block bb) {
+  tree mystring_tree;
+
+  char startstring[] = "[placeholder] This is a loop of %s\n";
+  mystring_tree = fix_string_type( build_string(strlen(startstring)+1,
+						startstring) );
+
+  tree mystring_type = build_pointer_type (TREE_TYPE(
+						     TREE_TYPE(mystring_tree)));
+  tree mystring_args_tree = build1(ADDR_EXPR, mystring_type, mystring_tree);
+
+  const char *fString = gimple_decl_printable_name(cfun->decl, 3);
+  tree fString_tree = fix_string_type( build_string(strlen(fString)+1,
+						    fString) );
+  tree fString_type = build_pointer_type (
+					  TREE_TYPE(TREE_TYPE(fString_tree)));
+  tree fString_args_tree = build1(ADDR_EXPR, fString_type, fString_tree);
+
+  tree fn = builtin_decl_implicit(BUILT_IN_PRINTF);
+  gimple print_gimple = gimple_build_call(fn, 2, 
+					  mystring_args_tree,
+					  fString_args_tree);
+  
+  gimple_stmt_iterator gsi = gsi_start_bb(bb);
+  gsi_insert_before(&gsi, print_gimple, GSI_NEW_STMT);
+
+}
+
+void analyze_fn_loops(struct function *fn) {
+  const char *fn_name = fndecl_name(fn->decl);
+
+  struct loop *cLoop = get_loop(fn, 0);
+  FOR_EACH_LOOP(cLoop, 0) {
+    printf("[%s] loop detected\n", fn_name);
+
+    // Use ->src to add before start of loop, ->dest to add inside start of loop
+    basic_block preLoop = loop_preheader_edge(cLoop)->src;
+    placeholder(preLoop);
+  }
+
+  return;
+}
 
 const pass_data loop_analysis_pass_data =
   {
@@ -136,12 +187,12 @@ public:
     : gimple_opt_pass(loop_analysis_pass_data, ctxt) {}
 
   bool gate() { 
-    std::cerr << "Entering gate\n";
+    std::cerr << "[analysis_pass] Entering gate\n";
     const char *current_function = fndecl_name(cfun->decl);
 
     if ( function_to_check(current_function) ) {
       const char *current_function = fndecl_name(cfun->decl);
-      printf("Analyzing function %s\n", current_function);
+      printf("[analysis_pass] Analyzing function %s\n", current_function);
 
       return true;
     }
@@ -150,7 +201,8 @@ public:
   }
 
   unsigned int execute() {
-    
+    //struct loops *loop_start = loops_for_fn(cfun);
+    analyze_fn_loops(cfun);
 
     return 0;
   }
@@ -162,7 +214,7 @@ public:
 
 int plugin_init (struct plugin_name_args *plugin_info,
 		 struct plugin_gcc_version *version ) {
-  printf("Vectorization plugin loaded!\n");
+  printf("[init] Vectorization plugin loaded!\n");
 
   // Declare pragmas
   register_callback(plugin_info->base_name,
--- tests/basic_loop.c
+++ tests/basic_loop.c
@@ -1,15 +1,28 @@
-
+#include <stdio.h>
 
 #pragma MIHPS vcheck dummy
 void dummy() {
   int i;
   int sum = 0;
+
   for ( i = 0 ; i < 10 ; ++i ) {
     sum += i;
   }
+
+  /*
+  for ( i = 0 ; i < 10 ; ++i ) {
+    printf("%d\n", i);
+  }
+  printf("\n");
+  */
+
+  printf("%d\n", sum);
+  
   return;
 }
 int main(void) {
+  printf("Beginning basic_loop test\n");
+
   dummy();
 
   return 0;
